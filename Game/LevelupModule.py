0debb4aa2561b674b5ad91a99bfb502c88360dd6
SCP-939-83 <512707@vistacollege.nl>
2022-12-09 14:15:41 +0100

    update level module

--- Game/LevelupModule.py
+++ Game/LevelupModule.py
@@ -7,28 +7,31 @@ def level_up(level: int, xp: int, xp_level: int) -> int:
 
     xp -= xp_level
 
-    xp_level = int(xp_level * 2)
+    xp_level = int(xp_level * 1.000000000000000000000000005)
  
   return level, xp, xp_level
 
 
 level = 1
-xp = 110
+xp = 1100000000000
 xp_level = 100
+combat = 1
+while combat == 1:
+  level, xp, xp_level = level_up(level, xp, xp_level)
 
 
-level, p, xp_level = level_up(level, xp, xp_level)
+  print(f"Player's level: {level}")
+  print(f"Player's current XP: {xp}")
+  print(f"Player's XP needed to reach the next level: {xp_level}")
 
 
-print(f"Player's level: {level}")
-print(f"Player's current XP: {xp}")
-print(f"Player's XP needed to reach the next level: {xp_level}")
+  player = 100
+  new_level = 1
+  if level >= new_level:
+    new_level = int(new_level + 1)
+    player = int(player * 1.1)
 
-
-player = 100
-new_level = 1
-if level >= new_level:
-  new_level = int(new_level + 1)
-  player = int(player * 1.1)
-print(player)
-
+  print(player)
+  print(new_level)
+  if xp_level >= xp:
+    break
