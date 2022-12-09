340df4cfbbd4a0af499ac2d9fce314a370cfd63b
HetGameBoekje <514527@vistacollege.nl>
2022-12-09 09:43:35 +0100

    Update ModuleLocation.py

--- Game/ModuleLocation.py
+++ Game/ModuleLocation.py
@@ -1,6 +1,7 @@
 #player is at village and has to go to the forest
 
 def locationVillage():
+    global location
     print("You are at the village")
     print("You can go to the forest or the mountain")
     print("Which one do you choose?")
@@ -18,6 +19,7 @@ def locationVillage():
 
 
 def locationForest():
+    global location
     print("You are in the forest")
     print("You can go to the village or the mountain")
     print("Which one do you choose?")
@@ -34,6 +36,7 @@ def locationForest():
 
 
 def locationMountain():
+    global location
     print("You are in the mountain")
     print("You can go to the village or the forest")
     print("Which one do you choose?")
@@ -48,20 +51,23 @@ def locationMountain():
         print("Invalid input! Game closes!")
         exit()
 
+def startGame():
+    global location
+    print("Hello player, welcome to the game!")
+    location = input("Do you want to go to the village, forest or mountain? (V/F/M): ")
+    if location == "V":
+        locationVillage()
+    elif location == "F":
+        locationForest()
+    elif location == "M":
+        locationMountain()
+    elif location == "stop":
+        pass
+    else:
+        print("Invalid input! Game closes!")
+        exit()
 
-print("Hello player, welcome to the game!")
-location = input("Do you want to go to the village, forest or mountain? (V/F/M): ")
-if location == "V":
-    locationVillage()
-elif location == "F":
-    locationForest()
-elif location == "M":
-    locationMountain()
-elif location == "stop":
-    pass
-else:
-    print("Invalid input! Game closes!")
-    exit()
-
+startGame()
 print (location)
 input("Press enter to continue...")
+
