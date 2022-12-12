cf2dd2e62dc510a6694c2619a7b3e81398b80e4b
SCP-939-83 <512707@vistacollege.nl>
2022-12-12 09:30:32 +0100

    Update LevelupModule.py
    test

--- Game/LevelupModule.py
+++ Game/LevelupModule.py
@@ -10,7 +10,21 @@ def level_up(level: int, xp: int, xp_level: int) -> int:
     xp_level = int(xp_level * 2)
  
   return level, xp, xp_level
-
+def stats():
+  player = 100
+  new_level = 1
+  armor = 5
+  damage = 5
+  poison = 10
+  fire = 10
+  while level >= new_level:
+    new_level = int(new_level + 1)
+    player = (player * 1.1)
+    armor = (armor * 1.1)
+    damage = (damage * 1.1)
+    poison = (poison * 1.1)
+    fire = (fire * 1.1)
+  return player, new_level, armor, fire, damage, poison
 
 level = 1
 xp = 11000000
@@ -18,20 +32,14 @@ xp_level = 100
 combat = 1
 while combat == 1:
   level, xp, xp_level = level_up(level, xp, xp_level)
-
-
-  print(f"Player's level: {level}")
-  print(f"Player's current XP: {xp}")
-  print(f"Player's XP needed to reach the next level: {xp_level}")
-
-
-  player = 100
-  new_level = 1
-  if level >= new_level:
-    new_level = int(new_level + 1)
-    player = int(player * 1.1)
-
-  print(player)
-  print(new_level)
-  if xp_level >= xp:
-    break
+  player, new_level, armor, damage, poison, fire = stats()
+  print(f"Player's level:", level)
+  print(f"Player's current XP:", xp)
+  print(f"Player's XP needed to reach the next level:", xp_level)
+  print(f"HP:",player)
+  print(f"Next level:",new_level)
+  print(f"armor:",armor)
+  print(f"damage:",damage)
+  print(f"poison damage:",poison)
+  print(f"fire damage:",fire)
+  break
