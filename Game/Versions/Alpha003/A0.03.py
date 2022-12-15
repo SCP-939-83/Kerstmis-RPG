c44272cb44a2c196029319d8b49491addf4acceb
HetGameBoekje <514527@vistacollege.nl>
2022-12-15 22:27:51 +0100

    building main game

--- Game/Versions/Alpha003/A0.03.py
+++ Game/Versions/Alpha003/A0.03.py
@@ -1,8 +1,19 @@
-import FightModule
-# import LevelupModule
-# import ModuleRandomINV
-# import ModuleLocation
+import loadsave
+# This import is for the module to save , load and create ONE playerfile
 
-print(FightModule.hpMonster)
+def test():
+    # loadsave.name = input("What is your name? ")
+    # loadsave.age = int(input("How old are you? "))
+    # loadsave.city = input("Where do you live? ")
+    loadsave.loadgame()
 
-
+test()
+# gamesave = input("Do you want to save this game? yes/no")
+# gamesave = gamesave.lower()
+# if gamesave == "yes":
+#     loadsave.savegame()
+#     loadsave.loadgame()
+# else:
+#     loadsave.loadgame()
+#     pass
+# print(4)
--- Game/Versions/Alpha003/loadsave.py
+++ Game/Versions/Alpha003/loadsave.py
@@ -13,9 +13,10 @@ class Person:
 
 def playerfile():
     global my_dict
-    Person.name = input("name:")
-    Person.age = int(input("age:"))
-    Person.city = input("city:")
+    global name, age, city
+    Person.name = name
+    Person.age = age
+    Person.city = city
     my_dict = {
         "name": Person.name,
         "age": Person.age,
@@ -67,16 +68,18 @@ def loadgame():
         data = json.load(f)
 
     # Print the contents of the dictionary
-    print(data["age"])
     global age , name , city
     age = data["age"]
     name = data["name"]
     city = data["city"]
-    print(name, age , city, "Are you sure you want to load this game?")
+    print("name = ",name)
+    print("age = ",age)
+    print("city = ",city)
+    print("loaded game successfully")
 
-
-for i in range(4):
-    loadgame()
-    playerfile()
-    savegame()  
-    
+if __name__ == "__main__":
+    for i in range(1):
+        loadgame()
+        playerfile()
+        savegame()  
+        
