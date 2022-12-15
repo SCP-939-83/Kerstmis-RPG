e21643d9ac89574e06eccebd7df5bf1d6e9e7e28
HetGameBoekje <514527@vistacollege.nl>
2022-12-15 17:56:36 +0100

    Loadgame works

--- Game/Versions/Alpha003/test.py
+++ Game/Versions/Alpha003/test.py
@@ -11,29 +11,48 @@ my_dict = {
     "city": city
 }
 
-# Define the directory where the file should be saved
-my_dir = "Game\Versions\Alpha003"
 
-# Use the os.makedirs() method to create the directory if it doesn't exist
-os.makedirs(my_dir, exist_ok=True)
+def savegame():
+    global data
+    # Define the directory where the file should be saved
+    my_dir = "Game\Versions\Alpha003"
 
-# Use the os.path.join() method to combine the directory and file name
-file_path = os.path.join(my_dir, "my_file.json")
+    # Use the os.makedirs() method to create the directory if it doesn't exist
+    os.makedirs(my_dir, exist_ok=True)
 
-# Open the file for writing, and write the JSON string to the file
-with open(file_path, "w") as f:
-    json.dump(my_dict, f)
+    # Use the os.path.join() method to combine the directory and file name
+    file_path = os.path.join(my_dir, "my_file.json")
 
-# Open the JSON file for reading and writing
-with open("my_file.json", "r+") as f:
-    # Load the JSON string from the file
-    data = json.load(f)
+    # Open the file for writing, and write the JSON string to the file
+    with open(file_path, "w") as f:
+        json.dump(my_dict, f)
 
-    # Increase the value of the "age" field
-    data["age"] += 1
+    # Open the JSON file for reading and writing
+    with open("my_file.json", "r+") as f:
+        # Load the JSON string from the file
+        data = json.load(f)
+        # Move the file pointer to the beginning of the file
+        f.seek(0)
 
-    # Move the file pointer to the beginning of the file
-    f.seek(0)
+        # Write the updated dictionary to the file
+        json.dump(data, f)
 
-    # Write the updated dictionary to the file
-    json.dump(data, f)
+
+def loadgame():
+    my_dir = "Game\Versions\Alpha003"
+
+    # Use the os.makedirs() method to create the directory if it doesn't exist
+    os.makedirs(my_dir, exist_ok=True)
+
+    # Use the os.path.join() method to combine the directory and file name
+    file_path = os.path.join(my_dir, "my_file.json")
+
+    # Open the file for reading
+    with open(file_path, "r") as f:
+        # Load the JSON string from the file and convert it to a dictionary
+        data = json.load(f)
+
+    # Print the contents of the dictionary
+    print(data)
+
+loadgame()
