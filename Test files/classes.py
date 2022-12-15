940a1e1a78791d0a971220560ade3cf3b035483a
HetGameBoekje <514527@vistacollege.nl>
2022-12-15 09:05:39 +0100

    classes test file

--- Test files/classes.py
+++ Test files/classes.py
@@ -1,5 +1,5 @@
 class MyClass:
-  x = "cheese"
+  x = 5
 
 p1 = MyClass()
 print(p1.x)
@@ -9,8 +9,22 @@ class Person:
     self.name = name
     self.age = age
     self.level = level
-
-p1 = Person("John", 36)
-
+x = 5
+y = 1
+p1 = Person("John", x, y)
 print(p1.name)
 print(p1.age)
+print(p1.level)
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
