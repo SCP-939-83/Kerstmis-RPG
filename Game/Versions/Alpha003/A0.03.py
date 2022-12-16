e81af8c70a26537a64504a8e60f1a67f72e7014c
HetGameBoekje <514527@vistacollege.nl>
2022-12-16 09:17:49 +0100

    json update

--- Game/Versions/Alpha003/A0.03.py
+++ Game/Versions/Alpha003/A0.03.py
@@ -6,6 +6,9 @@ gamesave.playerfilecheck()
 match gamesave.level:
     case 1:
         print("You entered the first level")
-
+    case 2:
+        print("You entered the second level")
+    case 3:
+        print("You entered the third level")
     case _:
         print("You are in an unknown level")
--- Game/Versions/Alpha003/gamesave.py
+++ Game/Versions/Alpha003/gamesave.py
@@ -1,19 +1,17 @@
 import json
 import os
-# Define a Python dictionary
 my_dir = "Game\Versions\Alpha003"
 
-    # Use the os.makedirs() method to create the directory if it doesn't exist
 os.makedirs(my_dir, exist_ok=True)
-# Use the os.path.join() method to combine the directory and file name
 file_path = os.path.join(my_dir, "my_file.json")
 
 class Person:
-    def __init__(self, name, age, city, level):
+    def __init__(self, name, age, city, level , xp):
         self.name = name
         self.age = age
         self.city = city
         self.level = level
+        self.xp = xp
 
 
 def playerfile():
@@ -22,11 +20,13 @@ def playerfile():
     Person.age = int(input("age:"))
     Person.city = input("city:")
     Person.level = 1
+    Person.xp = 0
     my_dict = {
         "name": Person.name,
         "age": Person.age,
         "city": Person.city,
         "level" : Person.level,
+        "xp" : Person.xp,
     }
     return my_dict
 
@@ -35,27 +35,19 @@ def playerfile():
 
 def savegame():
     global data
-    # Define the directory where the file should be saved
     my_dir = "Game\Versions\Alpha003"
 
-    # Use the os.makedirs() method to create the directory if it doesn't exist
     os.makedirs(my_dir, exist_ok=True)
 
-    # Use the os.path.join() method to combine the directory and file name
     file_path = os.path.join(my_dir, "my_file.json")
 
-    # Open the file for writing, and write the JSON string to the file
     with open(file_path, "w") as f:
         json.dump(my_dict, f)
 
-    # Open the JSON file for reading and writing
     with open("my_file.json", "r+") as f:
-        # Load the JSON string from the file
         data = json.load(f)
-        # Move the file pointer to the beginning of the file
         f.seek(0)
 
-        # Write the updated dictionary to the file
         json.dump(data, f)
 
 
@@ -63,18 +55,13 @@ def loadgame():
     global file_path
     my_dir = "Game\Versions\Alpha003"
 
-    # Use the os.makedirs() method to create the directory if it doesn't exist
     os.makedirs(my_dir, exist_ok=True)
 
-    # Use the os.path.join() method to combine the directory and file name
     file_path = os.path.join(my_dir, "my_file.json")
 
-    # Open the file for reading
     with open(file_path, "r") as f:
-        # Load the JSON string from the file and convert it to a dictionary
         data = json.load(f)
 
-    # Print the contents of the dictionary
     print(data["age"])
     global age , name , city , level
     age = data["age"]
