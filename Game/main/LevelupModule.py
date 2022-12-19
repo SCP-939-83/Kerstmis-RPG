f9e18d103441f0b6350c97c73caeca6f64ff06d5
HetGameBoekje <514527@vistacollege.nl>
2022-12-19 09:37:01 +0100

    added maingame

---
+++ Game/main/LevelupModule.py
@@ -0,0 +1,90 @@
+level = 1
+xp = 200
+skill_points = 0
+
+BASE_HP = 100
+HP_PER_LEVEL = 10
+
+BASE_DAMAGE = 10
+DAMAGE_PER_LEVEL = 5
+
+BASE_ARMOR = 0
+ARMOR_PER_LEVEL = 2
+
+BASE_CHARISMA = 0
+CHARISMA_PER_LEVEL = 1
+
+BASE_INTELLIGENCE = 0
+INTELLIGENCE_PER_LEVEL = 1
+
+skills = {
+  "HP": 0,
+  "Damage": 0,
+  "Armor": 0,
+  "Charisma": 0,
+  "Intelligence": 0
+}
+
+def level_up():
+  global level, skill_points, next_level_up_xp
+
+  next_level_up_xp = 100 * (level+1)
+  
+
+  if xp >= next_level_up_xp:
+
+    level += 1
+    skill_points += 1
+
+    print(f"Congratulations! You are now level {level} and have {skill_points} skill points to spend.")
+  else:
+
+    print(f"You need {next_level_up_xp} experience points to reach level {level+1}.")
+
+def spend_skill_point():
+  global skill_points
+
+  if skill_points > 0:
+    print("Which skill do you want to level up?")
+    for skill in skills:
+      print(f"- {skill} (level {skills[skill]})")
+    skill = input("> ")
+    
+
+    if skill in skills:
+
+      skills[skill] += 1
+      skill_points -= 1
+
+      print(f"You have spent a skill point on {skill}. You have {skill_points} skill points remaining.")
+      if skill == "HP":
+        print("You have gained 10 HP")
+      elif skill == "Damage":
+        print("You have gained 1 Damage")
+      elif skill == "Armor":
+        print("You have gained 10 Armor")
+      elif skill == "Charisma":
+        print("You have gained 1 Charisma")
+      elif skill == "Intelligence":
+        print("You have gained 1 Intelligence")
+    else:
+
+      print(f"You don't have the skill {skill}.")
+  else:
+
+    print("You don't have enough skill points to spend.")
+
+level_up()
+spend_skill_point()
+
+total_hp = BASE_HP + HP_PER_LEVEL * skills["HP"]
+total_damage = BASE_DAMAGE + DAMAGE_PER_LEVEL * skills["Damage"]
+total_armor = BASE_ARMOR + ARMOR_PER_LEVEL * skills["Armor"]
+total_charisma = BASE_CHARISMA + CHARISMA_PER_LEVEL * skills["Charisma"]
+total_intelligence = BASE_INTELLIGENCE + INTELLIGENCE_PER_LEVEL * skills["Intelligence"]
+
+print(f"Total HP: {total_hp}")
+print(f"Total Damage: {total_damage}")
+print(f"Total Armor: {total_armor}")
+print(f"Total Charisma: {total_charisma}")
+print(f"Total Intelligence: {total_intelligence}")
---
+++ Game/main/ModuleRandomINV.py
@@ -0,0 +1,89 @@
+import random
+inventory = []
+def itemgen():
+    z = random.randint(1, 1000)
+    global inventory
+    if z >= 1 and z <= 5:
+        loot = ("\033[1;31m Sword ")
+        inventory.append("Sword")
+    elif z >= 6 and z <= 25:
+        loot = ("\033[1;33m mushroom ")
+        inventory.append("mushroom")
+    elif z >= 26 and z <= 75:
+        loot = ("\033[1;35m flower ")
+        inventory.append("flower")
+    elif z >= 76 and z <= 200:
+        loot = ("\033[1;34m potato ")
+        inventory.append("potato")
+    elif z >= 201 and z <= 500:
+        loot = ("\033[1;32m rock ")
+        inventory.append("rock")
+    elif z >= 501 and z <= 1000:
+        loot = ("\033[1;0m stick ")
+        inventory.append("stick")
+    print("\033[1;0m Je hebt een" + loot + "\033[1;0mitem gekregen!!")
+    return inventory
+
+def inventoryshow():
+    
+    stick = inventory.count("stick") 
+    rock = inventory.count("rock")
+    potato = inventory.count("potato")
+    flower = inventory.count("flower")
+    mushroom = inventory.count("mushroom")
+    Sword = inventory.count("Sword")
+
+    y = len(inventory)
+    print(stick, "sticks")
+    print(rock, "rocks")
+    print(potato, "potatoes")
+    print(flower, "flowers")
+    print(mushroom, "mushrooms")
+    print(Sword, "Sword")
+    print(y, "total items")
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    inventory.sort()
+    inventory.reverse()
+    print(inventory)
+
+
+
+if __name__ == "__main__":
+    # Geen while true maar duidelijk zijn
+    while True:
+        try:
+            integer = int(input("Hoeveel items wil je hebben?: "))
+        except ValueError:
+            print("Vul een getal in, geen letters of tekens!")
+            continue
+        else:
+            print(f'Je vulde dit getal in: {integer}')
+            break
+
+    for i in range(integer):
+        itemgen()
+
+
+    stick = inventory.count("stick") 
+    rock = inventory.count("rock")
+    potato = inventory.count("potato")
+    flower = inventory.count("flower")
+    mushroom = inventory.count("mushroom")
+    apple = inventory.count("apple")
+
+    y = len(inventory)
+    print(stick, "sticks")
+    print(rock, "rocks")
+    print(potato, "potatoes")
+    print(flower, "flowers")
+    print(mushroom, "mushrooms")
+    print(apple, "apples")
+    print(y, "total items")
+    print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    inventory.sort()
+    inventory.reverse()
+    print(inventory)
+
+    # z = random.randint(1, 1000)
+    # if z >= 1 and z <= 1000: loot = ("\033[1;31m mythical ")
+
