b92b484b8e7f4ffce73b6053ae452122ff1d1edf
HetGameBoekje <514527@vistacollege.nl>
2022-12-21 19:57:51 +0100

    major update

--- Game/main/main.py
+++ Game/main/main.py
@@ -13,6 +13,22 @@ inventory = ModuleRandomINV.inventory
 gamesave.inventory = ModuleRandomINV.inventory + gamesave.inventory
 gamesave.karma = FightModule.karma + gamesave.karma
 
+def shoppingcheck():
+    global savelevel
+    if ModuleLocation.shopping <= 0:
+        print("Maybe you should go back to the store. You didnt get anything yet")
+        time.sleep(1.3)
+        ModuleLocation.locationStore()
+        shoppingcheck()
+    elif ModuleLocation.shopping >= 1:
+        print("You passed on to home...")
+        savelevel += 1
+        main()
+    else:
+        savelevel += 1
+        pass
+    return level
+
 
 def main():
     match savelevel:
@@ -76,7 +92,8 @@ the moment you are coming home, they hear a honking car and a big bang. They loo
                     print("Invalid choice. Please choose a valid task to help with.")
             print("You have completed all three tasks and learned more about the history of the village and the tree. It was a fulfilling day spent helping out and learning new things.")
             if tasks_completed > 5:
-                gamesave.savelevel == 6
+                gamesave.savelevel = 6
+                gamesave.savegame
             elif tasks_completed > 2 and tasks_completed < 5:
                 print("You helped some villagers, but not enough. Would you like to help more villagers?")
                 helpVillagers = input("Yes / No")
@@ -84,7 +101,7 @@ the moment you are coming home, they hear a honking car and a big bang. They loo
                 if helpVillagers == "yes":
                     main()
                 else:
-                    gamesave.savelevel == 5
+                    gamesave.savelevel = 5
             gamesave.savegame()
             main()
         case 5:
@@ -98,44 +115,71 @@ the moment you are coming home, they hear a honking car and a big bang. They loo
                 print("You try to call for help, but realize that you have no cellular data available.")
                 print("You fight the homeless person, but are severely injured in the process.")
                 FightModule.Endbattle()
-                if FightModule.battle_result == "win":
-                    
-                    print("You manage to survive the fight, but are left with severe injuries.")
-                    gamesave.savelevel == 7
-                    gamesave.savegame()
-                    main()
-                else:
-                    exit()
+                print("You manage to survive the fight, but are left with severe injuries.")
+                gamesave.savelevel = 7
+                gamesave.savegame()
+                main()
             else: 
                 print("8")
                 gamesave.savelevel = 8
                 print(gamesave.savelevel)
                 gamesave.savegame()
                 main()
+        case 6:
+            print("You have given away all your presents and managed to keep the homeless person at bay.")
+            print("As you walk around the village, you notice the old, decrepit tree in the center of the village. You remember hearing stories about its history and realize that it's time for it to be taken down.")
+            cut_tree = input("Do you want to cut down the evil tree? (yes/no) ")
+            if cut_tree == "yes":
+                print("You decide to cut down the evil tree.")
+                print("With determination and strength, you manage to bring down the old tree. The villagers cheer and thank you for your bravery and selflessness.")
+            else:
+                print("You decide not to cut down the tree and leave it be.")
+            print("It's the end of the holiday season and you feel a sense of accomplishment and joy from being able to bring happiness to those around you.")
+            print("Thank you for playing! The game has now ended.")
+        case 7:
+            print("You are severely injured and unable to fight back against the homeless person.")
+            print("As the homeless person approaches you with the intention of killing you, you realize that you have one last chance to protect the village.")
+            print("With all the strength you have left, you manage to kamikaze the homeless person, sacrificing yourself in the process.")
+            print("You die, but your bravery and selflessness have saved the village from further harm.")
+            print("Thank you for playing. The game has now ended.")
+        case 8:
+            print("You have become consumed by evil and decide to turn against the villagers.")
+            for i in range(17):
+                FightModule.option()
+            if FightModule.karma > 20:
+                print("You have become too evil and are no longer able to continue on this path.")
+                print("Your actions have caused harm to others and brought misery to the village.")
+                print("As a result, your save game is reset and you are given the opportunity to start anew and make better choices.")
+                gamesave.savelevel = 1
+                main()
+            elif FightModule.karma < 10:
+                print("You have not fully embraced your evil side and are not deemed worthy of staying in the village.")
+                print("You are kicked out of the village and are unable to return.")
+    
+                gamesave.savelevel = 100
+                main()
+            elif FightModule.karma == 0:
+                print("You have managed to maintain a balance between good and evil, and your actions have brought happiness to the village.")
+                print("Everyone is grateful for your help and kindness, and you are rewarded with a special message:")
+                print("'You are a true hero, and your actions will always be remembered in the village.'")
+                print("Thank you for playing. The game has now ended on a positive note.")
+            else:
+                print("You have successfully become evil and have caused harm to others.")
+                print("However, your actions have not gone unnoticed and you are eventually brought to justice for your crimes.")
+                print("Thank you for playing. The game has now ended.")
+
         case _:
             print("You are in an unknown level")
             for i in range(5):
                 time.sleep(1)
                 print("ZZzzz....")
             time.sleep(1)
+            reset = input("Reset gamelevel?")
+            if reset == "yes":
+                gamesave.savelevel = 1
+                gamesave.savegame()
+                main()
+            else: 
+                print("Maybe later...")
             main()
-
-
-def shoppingcheck():
-    global savelevel
-    if ModuleLocation.shopping <= 0:
-        print("Maybe you should go back to the store. You didnt get anything yet")
-        time.sleep(1.3)
-        ModuleLocation.locationStore()
-        shoppingcheck()
-    elif ModuleLocation.shopping >= 1:
-        print("You passed on to home...")
-        savelevel += 1
-        main()
-    else:
-        savelevel += 1
-        pass
-    return level
-
-
 main()
